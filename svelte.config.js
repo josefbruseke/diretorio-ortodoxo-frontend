ae61f4d0c3a255dc363342f10ea834e298bc0907
josefbruseke <josefbruseke@gmail.com>
2025-10-11 22:55:08 -0300

    Implement code structure updates and remove redundant code blocks

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,14 +8,8 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-static for cPanel deployment
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
+		// adapter-vercel for Vercel deployment
+		adapter: adapter()
 	}
 };
 
